185fb265daa02fd8659b83795a579c7c5ae26866
Dan Canetti <info@danielcanetti.com>
2020-09-01 15:44:04 +0100

    Trying Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@
 FROM ubuntu:latest
 
 # Run node install
-#RUN apt-get update
-#RUN apt-get -y install nodejs
+RUN apt-get update
+RUN apt-get -y install nodejs
 RUN apt-get -y install npm
 RUN apt-get -y install build-essential
+RUN mkdir eleventy-site
+RUN cd eleventy-site
+RUN npm install @11ty/eleventy
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY create.sh /create.sh
--- create.sh
+++ create.sh
@@ -1,17 +1,9 @@
 #!/bin/sh -l
 
-apt-get -y install nodejs
-apt-get -y install npm
-mkdir eleventy-site
-echo "Directoy created."
 cd eleventy-site
-npm install @11ty/eleventy
 echo "Eleventy installed."
-cd ../
 touch .eleventyignore
 touch .eleventy.js
 touch README.md
 echo "Files created."
-mv .eleventyignore ./eleventy-site && mv .eleventy.js ./eleventy-site && mv README.md ./eleventy-site
-echo "Files moved into new-eleventy-site."
 echo "Eleventy site created."
